7a5fcb19b6474449b2a790a28a0a2327a293a92e
hesy87 <hesam.mahmoodi@gmail.com>
2024-02-21 15:48:26 +0100

    state update

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react"
-import { useDispatch } from "react-redux"
-import { actionCreators } from "../state"
+import { useActions } from "../hooks/useActions"
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
-  const dispatch = useDispatch()
+  const { searchRepositories } = useActions();
 
   const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    dispatch(actionCreators.SearchRepositories(term) as any)
+    searchRepositories(term)
   }
   return (
     <div>
